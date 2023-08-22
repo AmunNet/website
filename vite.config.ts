b247f3f62ce328dc0429fb6474c607a9eb5dc7a3
HighPriest <git_user@amun.pl>
2023-08-22 02:47:02 +0200

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import icons from 'unplugin-icons/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 
-const config: UserConfig = {
-	plugins: [icons({ compiler: 'svelte' }), sveltekit()],
+export default defineConfig({
+	plugins: [sveltekit()],
 	server: {
 		port: 3000
 	},
 	ssr: {
 		noExternal: process.env.NODE_ENV === 'production' ? ['@carbon/charts'] : []
-	}
-};
-
-export default config;
+	},
+});
